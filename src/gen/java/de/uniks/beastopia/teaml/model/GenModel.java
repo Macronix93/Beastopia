ec27143b5099c53fef61bdf0559ec0485688e6c9
Jonas Graunitz <jonas@graunitz.com>
2023-04-28 11:28:52 +0200

    feat: created GenModel.java

--- src/gen/java/de/uniks/beastopia/teaml/model/GenModel.java
+++ src/gen/java/de/uniks/beastopia/teaml/model/GenModel.java
@@ -1,4 +1,64 @@
 package de.uniks.beastopia.teaml.model;
 
-public class GenModel {
+import org.fulib.builder.ClassModelDecorator;
+import org.fulib.builder.ClassModelManager;
+import org.fulib.builder.reflect.Link;
+
+import java.util.List;
+
+public class GenModel implements ClassModelDecorator {
+
+    public class User {
+        String createdAt;
+        String updatedAt;
+        String name;
+        String status;
+        String avatar;
+
+        @Link
+        List<User> friends;
+        @Link("members")
+        List<Group> groups;
+    }
+
+    public class LoginResult {
+        String accessToken;
+        String refreshToken;
+
+        @Link
+        User user;
+    }
+
+    public class Message {
+        String createdAt;
+        String updatedAt;
+        String id;
+        String body;
+
+        @Link
+        User sender;
+    }
+
+    public class Group {
+        String createdAt;
+        String updatedAt;
+        String id;
+        String name;
+
+        @Link("groups")
+        List<User> members;
+    }
+
+    public class Region {
+        String createdAt;
+        String updatedAt;
+        String id;
+        String name;
+    }
+
+    @Override
+    public void decorate(ClassModelManager mm) {
+        mm.haveNestedClasses(GenModel.class);
+    }
+
 }
