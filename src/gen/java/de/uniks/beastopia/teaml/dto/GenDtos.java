5196fe26c174509a0b492b992ffbf59f0a4f1c51
Jonas Graunitz <jonas@graunitz.com>
2023-04-28 11:49:09 +0200

    feat: created GenDtos.java

--- src/gen/java/de/uniks/beastopia/teaml/dto/GenDtos.java
+++ src/gen/java/de/uniks/beastopia/teaml/dto/GenDtos.java
@@ -1,4 +1,55 @@
 package de.uniks.beastopia.teaml.dto;
 
-public class GenDtos {
+import de.uniks.beastopia.teaml.model.GenModel;
+import org.fulib.builder.ClassModelDecorator;
+import org.fulib.builder.ClassModelManager;
+import org.fulib.builder.Type;
+import org.fulib.builder.reflect.DTO;
+
+import java.util.List;
+
+public class GenDtos implements ClassModelDecorator {
+    @Override
+    public void decorate(ClassModelManager m) {
+        m.getClassModel().setDefaultPropertyStyle(Type.POJO);
+        m.haveNestedClasses(GenDtos.class);
+    }
+
+    @DTO(model = GenModel.User.class, pick = {"name"})
+    class LoginDto {
+        String password;
+    }
+
+    @DTO(model = GenModel.LoginResult.class, pick = {"refreshToken"})
+    class RefreshDto {
+    }
+
+    @DTO(model = GenModel.User.class, pick = {"name", "avatar"})
+    class CreateUserDto {
+        String password;
+    }
+
+    @DTO(model = GenModel.User.class, pick = {"name", "status", "avatar"})
+    class UpdateUserDto {
+        List<GenModel.User> friends;
+        String password;
+    }
+
+    @DTO(model = GenModel.Group.class, pick = {"name"})
+    class CreateGroupDto {
+        List<GenModel.User> members;
+    }
+
+    @DTO(model = GenModel.Group.class, pick = {"name"})
+    class UpdateGroupDto {
+        List<GenModel.User> members;
+    }
+
+    @DTO(model = GenModel.Message.class, pick = {"body"})
+    class CreateMessageDto {
+    }
+
+    @DTO(model = GenModel.Message.class, pick = {"body"})
+    class UpdateMessageDto {
+    }
 }
