3cd116dcbc6167c984b431031a1683e7eff56a5a
Celina Werkmeister <uk083378@student.uni-kassel.de>
2023-04-29 09:35:12 +0200

    feat: add Main and App

---
+++ src/main/java/de/uniks/beastopia/teaml/App.java
@@ -0,0 +1,11 @@
+package de.uniks.beastopia.teaml;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
