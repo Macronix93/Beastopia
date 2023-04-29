97676e11683319a562764f6ef9e7b60d6c917a62
Celina Werkmeister <uk083378@student.uni-kassel.de>
2023-04-29 09:53:38 +0200

    feat: Stage init

--- src/main/java/de/uniks/beastopia/teaml/App.java
+++ src/main/java/de/uniks/beastopia/teaml/App.java
@@ -1,11 +1,37 @@
 package de.uniks.beastopia.teaml;
 
+import de.uniks.beastopia.teaml.controller.Controller;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private Stage stage;
+    private Controller controller;
+
+    public App() {
+
+    }
+
+    public App(Controller controller) {
+        this.controller = controller;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        stage.setWidth(800);
+        stage.setHeight(600);
+        stage.setTitle("Beastopia");
 
+        final Scene scene = new Scene(new Label("Loading..."));
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
