cf0c024a6bbf0be91af49a56b432fe2da3377a7e
Armin Kozik <armin.kozik@gmx.net>
2023-04-30 14:59:24 +0200

    chore: add button fields

---
+++ src/main/java/de/uniks/beastopia/teaml/controller/PauseController.java
@@ -0,0 +1,12 @@
+package de.uniks.beastopia.teaml.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PauseController extends Controller {
+
+    @FXML
+    private Button editProfileButton;
+    @FXML
+    private Button backtoMainButton;
+}
