e1d1d1619b2993c7b058a88f91d658974f101487
Jonas Graunitz <jonas@graunitz.com>
2023-04-30 17:23:53 +0200

    feat: added field for hbox

--- src/main/java/de/uniks/beastopia/teaml/controller/IngameController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/IngameController.java
@@ -3,6 +3,7 @@ package de.uniks.beastopia.teaml.controller;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -10,7 +11,13 @@ import java.io.IOException;
 public class IngameController extends Controller {
 
     @FXML
+    public HBox ingame;
+    @FXML
     private Button pause;
+
+    @Inject
+    public IngameController() {
+    }
 
     public void pauseMenu() {
         //ToDo Show Pause Menu
@@ -18,10 +25,6 @@ public class IngameController extends Controller {
 
     public String getTitle() {
         return ("Beastopia");
-    }
-
-    @Inject
-    public IngameController() {
     }
 
     @Override
