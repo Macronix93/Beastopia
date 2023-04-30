f936605aa2bcd0267a9e21ce99e105419f8667b5
Armin Kozik <armin.kozik@gmx.net>
2023-04-30 19:40:05 +0200

    chore: add several methods

--- src/main/java/de/uniks/beastopia/teaml/controller/PauseController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/PauseController.java
@@ -1,7 +1,12 @@
 package de.uniks.beastopia.teaml.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+
+import javax.inject.Inject;
+import java.io.IOException;
 
 public class PauseController extends Controller {
 
@@ -9,4 +14,30 @@ public class PauseController extends Controller {
     private Button editProfileButton;
     @FXML
     private Button backToMainButton;
+    @Inject
+    public PauseController() {
+
+    }
+    public String getTitle() {
+        return ("Beastopia - Pause Menu");
+    }
+    @Override
+    public Parent render() {
+        final Parent parent;
+        parent = super.render();
+        /*
+        try {
+            parent = super.render();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        */
+        return parent;
+    }
+
+    public void editProfileButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void mainMenuButtonPressed(ActionEvent actionEvent) {
+    }
 }
