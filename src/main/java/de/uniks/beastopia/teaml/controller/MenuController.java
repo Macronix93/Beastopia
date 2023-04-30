413a8521cdfdd3f7dc10499c25107055f726a464
Armin Kozik <armin.kozik@gmx.net>
2023-04-30 11:27:31 +0200

    chore: Add inherit from parent class

--- src/main/java/de/uniks/beastopia/teaml/controller/MenuController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/MenuController.java
@@ -3,7 +3,7 @@ package de.uniks.beastopia.teaml.controller;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 
-public class MenuController {
+public class MenuController extends Controller {
     private Controller RegionController;
     private Controller FriendsController;
     @FXML
