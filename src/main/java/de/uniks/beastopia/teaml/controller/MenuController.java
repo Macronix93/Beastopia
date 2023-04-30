c4b4ed838922840c1d5752a3bdc11e2ff1f3202c
Armin Kozik <armin.kozik@gmx.net>
2023-04-30 11:25:55 +0200

    feat: Add fields for subcontrollers and their containers

---
+++ src/main/java/de/uniks/beastopia/teaml/controller/MenuController.java
@@ -0,0 +1,14 @@
+package de.uniks.beastopia.teaml.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class MenuController {
+    private Controller RegionController;
+    private Controller FriendsController;
+    @FXML
+    private VBox friendListContainer;
+    @FXML
+    private VBox regionContainer;
+
+}
