b676602ed8483d7e3bbc78f94168b38c182f554a
Armin Kozik <armin.kozik@gmx.net>
2023-04-30 19:34:57 +0200

    chore: added several methods

--- src/main/java/de/uniks/beastopia/teaml/controller/MenuController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/MenuController.java
@@ -1,7 +1,11 @@
 package de.uniks.beastopia.teaml.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
+
+import javax.inject.Inject;
+import java.io.IOException;
 
 public class MenuController extends Controller {
     private Controller regionController;
@@ -10,5 +14,28 @@ public class MenuController extends Controller {
     private VBox friendListContainer;
     @FXML
     private VBox regionContainer;
+
+    @Inject
+    public MenuController() {
+
+    }
+
+    public String getTitle() {
+        return ("Beastopia - Main Menu");
+    }
+
+    @Override
+    public Parent render() {
+        final Parent parent;
+        parent = super.render();
+        /*
+        try {
+            parent = super.render();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        */
+        return parent;
+    }
 
 }
