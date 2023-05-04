9b84bdb821be9915113c707d568a940c7380f6a4
Jonas Graunitz <jonas@graunitz.com>
2023-05-04 10:44:00 +0200

    feat: added css structure

--- src/main/java/de/uniks/beastopia/teaml/App.java
+++ src/main/java/de/uniks/beastopia/teaml/App.java
@@ -2,10 +2,13 @@ package de.uniks.beastopia.teaml;
 
 import de.uniks.beastopia.teaml.controller.Controller;
 
+import fr.brouillard.oss.cssfx.CSSFX;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class App extends Application {
     private Stage stage;
@@ -32,6 +35,9 @@ public class App extends Application {
 
         final Scene scene = new Scene(new Label("Loading..."));
         stage.setScene(scene);
+
+        scene.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("views/styles.css")).toString());
+        CSSFX.start(scene);
 
         stage.show();
 
