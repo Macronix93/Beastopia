a6e3de03c0a276d9745908133e6ca71448550a0d
Celina Werkmeister <uk083378@student.uni-kassel.de>
2023-05-04 19:16:01 +0200

    feat: expand IngameController to pause with ESC

--- src/main/java/de/uniks/beastopia/teaml/controller/IngameController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/IngameController.java
@@ -1,12 +1,14 @@
 package de.uniks.beastopia.teaml.controller;
 
+import de.uniks.beastopia.teaml.App;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 
 import javax.inject.Inject;
-import java.io.IOException;
+import javax.inject.Provider;
 
 public class IngameController extends Controller {
 
@@ -14,6 +16,10 @@ public class IngameController extends Controller {
     public HBox ingame;
     @FXML
     private Button pause;
+    @Inject
+    App app;
+    @Inject
+    Provider<MenuController> menuControllerProvider;
 
     @Inject
     public IngameController() {
@@ -26,5 +32,11 @@ public class IngameController extends Controller {
     @Override
     public String getTitle() {
         return "Beastopia";
+    }
+
+    public void escape(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ESCAPE)) {
+            app.show(menuControllerProvider.get());
+        }
     }
 }
