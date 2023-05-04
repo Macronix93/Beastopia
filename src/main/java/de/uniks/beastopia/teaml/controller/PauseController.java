bd2a9d21e44c0ec1886e89f10c0bfd0f075f74ed
Armin Kozik <armin.kozik@gmx.net>
2023-05-04 20:21:22 +0200

    chore: edit border in fxml

--- src/main/java/de/uniks/beastopia/teaml/controller/PauseController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/PauseController.java
@@ -3,8 +3,8 @@ package de.uniks.beastopia.teaml.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javax.inject.Inject;
 
+import javax.inject.Inject;
 
 public class PauseController extends Controller {
 
@@ -12,13 +12,17 @@ public class PauseController extends Controller {
     private Button editProfileButton;
     @FXML
     private Button backToMainButton;
+
     @Inject
     public PauseController() {
 
     }
+
+    @Override
     public String getTitle() {
         return "Beastopia - Pause Menu";
     }
+
     public void editProfileButtonPressed(ActionEvent actionEvent) {
     }
 
