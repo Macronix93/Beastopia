fdb068ef6692865984388636f3ce937954cee8b6
Jonas Graunitz <jonas@graunitz.com>
2023-05-06 19:42:04 +0200

    feat: used dagger

--- src/main/java/de/uniks/beastopia/teaml/controller/MenuController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/MenuController.java
@@ -5,18 +5,24 @@ import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuController extends Controller {
-    private final Controller regionController;
+    @Inject
+    Provider<RegionController> regionControllerProvider;
     private Controller friendListController;
     @FXML
     private VBox friendListContainer;
     @FXML
     private VBox regionContainer;
 
+    List<Controller> subControllers = new ArrayList<>();
+
     @Inject
     public MenuController() {
-        regionController = new RegionController();
+
     }
 
     @Override
@@ -25,9 +31,16 @@ public class MenuController extends Controller {
     }
 
     @Override
+    public void init() {
+        super.init();
+        subControllers.add(regionControllerProvider.get());
+        onDestroy(() -> subControllers.forEach(Controller::destroy));
+    }
+
+    @Override
     public Parent render() {
         Parent parent = super.render();
-        regionContainer.getChildren().add(regionController.render());
+        regionContainer.getChildren().add(subControllers.get(0).render());
         return parent;
     }
 
