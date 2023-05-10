974584624c15aabcbb7412b851c580239b2180bd
Celina Werkmeister <uk083378@student.uni-kassel.de>
2023-05-10 08:12:10 +0200

    test: add IngameControllerTest.java

---
+++ src/test/java/de/uniks/beastopia/teaml/controller/IngameControllerTest.java
@@ -0,0 +1,50 @@
+package de.uniks.beastopia.teaml.controller;
+
+import de.uniks.beastopia.teaml.App;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import javax.inject.Provider;
+import java.util.ResourceBundle;
+
+import static java.awt.Event.ESCAPE;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class IngameControllerTest extends ApplicationTest {
+
+    @Mock
+    Provider<PauseController> pauseControllerProvider;
+
+    @Spy
+    App app;
+    @Spy
+    ResourceBundle resources = ResourceBundle.getBundle("de/uniks/beastopia/teaml/assets/lang");
+
+    @InjectMocks
+    IngameController ingameController;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        app.start(stage);
+        app.show(ingameController);
+    }
+
+    @Test
+    void pauseMenu() {
+        final PauseController mock = Mockito.mock(PauseController.class);
+        when(pauseControllerProvider.get()).thenReturn(mock);
+        doNothing().when(app).show(mock);
+
+        clickOn(String.valueOf(ESCAPE));
+
+        verify(app).show(mock);
+    }
+}
