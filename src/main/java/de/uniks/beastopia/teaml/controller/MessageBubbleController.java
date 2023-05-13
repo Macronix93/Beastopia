8d69c354ebcee86985a3c5528182d2e674589ff4
Celina Werkmeister <uk083378@student.uni-kassel.de>
2023-05-13 17:04:45 +0200

    feat: add method setupMessageBubbleController

--- src/main/java/de/uniks/beastopia/teaml/controller/MessageBubbleController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/MessageBubbleController.java
@@ -1,20 +1,21 @@
 package de.uniks.beastopia.teaml.controller;
 
+import de.uniks.beastopia.teaml.rest.Message;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.text.Text;
 
 import javax.inject.Inject;
+import java.util.Date;
 
 public class MessageBubbleController extends Controller {
     @FXML
-    public Text senderName;
+    public String senderName;
     @FXML
-    public Text messageBody;
+    public String messageBody;
     @FXML
-    public Text created;
+    public Date created;
     @FXML
-    public Text updated;
+    public Date updated;
     @FXML
     public Button editButton;
     @FXML
@@ -23,6 +24,14 @@ public class MessageBubbleController extends Controller {
     @Inject
     public MessageBubbleController() {
 
+    }
+
+    public MessageBubbleController setupMessageBubbleController(Message message) {
+        this.senderName = message.sender();
+        this.messageBody = message.body();
+        this.created = message.createdAt();
+        this.updated = message.updatedAt();
+        return this;
     }
 
     @FXML
