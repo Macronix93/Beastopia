e517bec2eb5a1b16c64f24acb0aa4a79575f1511
Celina Werkmeister <uk083378@student.uni-kassel.de>
2023-05-13 17:14:49 +0200

    feat: add MessageBubbleControllerTest.java

---
+++ src/test/java/de/uniks/beastopia/teaml/controller/MessageBubbleControllerTest.java
@@ -0,0 +1,39 @@
+package de.uniks.beastopia.teaml.controller;
+
+import de.uniks.beastopia.teaml.App;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import java.util.ResourceBundle;
+
+@ExtendWith(MockitoExtension.class)
+public class MessageBubbleControllerTest extends ApplicationTest {
+
+    @Spy
+    App app;
+    @Spy
+    ResourceBundle resources = ResourceBundle.getBundle("de/uniks/beastopia/teaml/assets/lang");
+
+    @InjectMocks
+    MessageBubbleController messageBubbleController;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        app.start(stage);
+        app.show(messageBubbleController);
+        stage.requestFocus();
+    }
+
+    @Test
+    void editMessage() {
+    }
+
+    @Test
+    void deleteMessage() {
+    }
+}
