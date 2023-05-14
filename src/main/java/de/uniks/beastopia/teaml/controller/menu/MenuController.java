e275806c0d9abb6d7fc828103b1ce21d688483c8
Luis Schrader <luisschr@protonmail.com>
2023-05-14 13:15:10 +0200

    feat: revisit main menu

--- src/main/java/de/uniks/beastopia/teaml/controller/menu/MenuController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/menu/MenuController.java
@@ -2,9 +2,14 @@ package de.uniks.beastopia.teaml.controller.menu;
 
 import de.uniks.beastopia.teaml.controller.Controller;
 import de.uniks.beastopia.teaml.controller.menu.social.FriendListController;
+import de.uniks.beastopia.teaml.rest.User;
+import de.uniks.beastopia.teaml.service.TokenStorage;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -13,15 +18,24 @@ import java.util.List;
 
 public class MenuController extends Controller {
     private final List<Controller> subControllers = new ArrayList<>();
-    @Inject
-    Provider<RegionController> regionControllerProvider;
-    @Inject
-    Provider<FriendListController> friendListControllerProvider;
+
     @FXML
     private VBox friendListContainer;
     @FXML
     private VBox regionContainer;
+    @FXML
+    private ImageView banner;
+    @FXML
+    private ImageView userAvatar;
+    @FXML
+    private Text userName;
 
+    @Inject
+    Provider<RegionController> regionControllerProvider;
+    @Inject
+    Provider<FriendListController> friendListControllerProvider;
+    @Inject
+    TokenStorage tokenStorage;
     @Inject
     public MenuController() {
 
@@ -30,6 +44,11 @@ public class MenuController extends Controller {
     @Override
     public Parent render() {
         Parent parent = super.render();
+        //set beastopia banner
+        //set userAvatar
+
+        userName.setText(tokenStorage.getCurrentUser().name());
+
         Controller friendListController = friendListControllerProvider.get();
         subControllers.add(friendListController);
         friendListContainer.getChildren().add(friendListController.render());
@@ -50,4 +69,13 @@ public class MenuController extends Controller {
         return "Beastopia - Main Menu";
     }
 
+    @FXML
+    public void editProfileButtonPressed() {
+        //app.show(editProfileControllerProvider.get());
+    }
+
+    @FXML
+    public void logout() {
+
+    }
 }
