e88bb09a101ff8ca4f32231d1beef64ebc52b4ba
Celina Werkmeister <uk083378@student.uni-kassel.de>
2023-05-14 10:44:46 +0200

    chore: add adjustments after merge conflict

--- src/main/java/de/uniks/beastopia/teaml/MainComponent.java
+++ src/main/java/de/uniks/beastopia/teaml/MainComponent.java
@@ -3,6 +3,7 @@ package de.uniks.beastopia.teaml;
 import dagger.BindsInstance;
 import dagger.Component;
 import de.uniks.beastopia.teaml.controller.auth.LoginController;
+import de.uniks.beastopia.teaml.controller.menu.MenuController;
 import de.uniks.beastopia.teaml.modules.HttpModule;
 import de.uniks.beastopia.teaml.modules.MainModule;
 import de.uniks.beastopia.teaml.modules.PrefModule;
