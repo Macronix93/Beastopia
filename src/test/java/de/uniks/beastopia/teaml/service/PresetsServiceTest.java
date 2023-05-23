a066693cfdd4feaf2afc6f940f92e6cb10f48b05
Akloib <ali.k0beissi.ak@gmail.com>
2023-05-23 22:13:05 +0200

    chore: add parts of PresetApi/Service/Test

---
+++ src/test/java/de/uniks/beastopia/teaml/service/PresetsServiceTest.java
@@ -0,0 +1,52 @@
+package de.uniks.beastopia.teaml.service;
+
+import de.uniks.beastopia.teaml.rest.presets.PresetsApiService;
+import io.reactivex.rxjava3.core.Observable;
+import okhttp3.MediaType;
+import okhttp3.ResponseBody;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PresetsServiceTest {
+
+    @Mock
+    PresetsApiService presetsApiService;
+
+    @InjectMocks
+    PresetsService presetsService;
+
+    List<String> allSprites = List.of("A.png", "B.png");
+
+    ResponseBody responseBody = ResponseBody.create(MediaType.parse("text/html"), "<h1>Fake</h1>");
+
+    @Test
+    void getCharacters() {
+        when(presetsApiService.getCharacters()).thenReturn(Observable.just(allSprites));
+
+        List<String> responseList = presetsService.getCharacters().blockingFirst();
+
+        assertEquals("A.png", responseList.get(0));
+        assertEquals("B.png", responseList.get(1));
+        verify(presetsApiService).getCharacters();
+    }
+
+    @Test
+    void getSpriteSheet() {
+        when(presetsApiService.getSpriteSheet("A.png")).thenReturn(Observable.just(responseBody));
+
+        ResponseBody response = presetsService.getSpriteSheet("A.png").blockingFirst();
+
+        assertEquals(13, response.contentLength());
+        verify(presetsApiService).getSpriteSheet("A.png");
+    }
+}
