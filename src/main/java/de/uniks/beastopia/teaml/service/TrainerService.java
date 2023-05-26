e18f6aabc81c5ba6e007de267c6f9f5a9e45abc5
Sylvan93 <macronix93@gmx.de>
2023-05-26 15:37:50 +0200

    chore: add trainer dto and trainer services

---
+++ src/main/java/de/uniks/beastopia/teaml/service/TrainerService.java
@@ -0,0 +1,37 @@
+package de.uniks.beastopia.teaml.service;
+
+import de.uniks.beastopia.teaml.rest.CreateTrainerDto;
+import de.uniks.beastopia.teaml.rest.Trainer;
+import de.uniks.beastopia.teaml.rest.TrainerApiService;
+import io.reactivex.rxjava3.core.Observable;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class TrainerService {
+    @Inject
+    TokenStorage tokenStorage;
+
+    @Inject
+    TrainerApiService trainerApiService;
+
+    @Inject
+    TrainerService() {
+    }
+
+    public Observable<Trainer> createTrainer(String name, String image) {
+        return trainerApiService.createTrainer(new CreateTrainerDto(name, image));
+    }
+
+    public Observable<List<Trainer>> getAllTrainer(String regionId) {
+        return trainerApiService.getAllTrainer(regionId);
+    }
+
+    public Observable<Trainer> getTrainer(String regionId, String trainerId) {
+        return trainerApiService.getTrainer(regionId, trainerId);
+    }
+
+    public Observable<Trainer> deleteTrainer(String regionId, String trainerId) {
+        return trainerApiService.deleteTrainer(regionId, trainerId);
+    }
+}
