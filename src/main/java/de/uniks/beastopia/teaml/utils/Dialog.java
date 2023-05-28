90666e1c2f08ab1488e8d6f16694e9941d4a9d26
Lonartie <gierschner.lg@gmail.com>
2023-05-28 15:34:38 +0200

    fix: Dialog.error for ValidationErrorResponse

--- src/main/java/de/uniks/beastopia/teaml/utils/Dialog.java
+++ src/main/java/de/uniks/beastopia/teaml/utils/Dialog.java
@@ -6,19 +6,25 @@ import de.uniks.beastopia.teaml.rest.ValidationErrorResponse;
 import javafx.scene.control.Alert;
 import retrofit2.HttpException;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class Dialog {
     public static void error(Throwable exception, String dialogHeader) {
         if (exception instanceof HttpException httpError) {
             String message;
+            String json;
             try {
-                String json = Objects.requireNonNull(Objects.requireNonNull(httpError.response()).errorBody()).string();
+                json = Objects.requireNonNull(Objects.requireNonNull(httpError.response()).errorBody()).string();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
                 ErrorResponse response = new Gson().fromJson(json, ErrorResponse.class);
                 message = response.message();
             } catch (Exception e) {
                 try {
-                    String json = Objects.requireNonNull(Objects.requireNonNull(httpError.response()).errorBody()).string();
                     ValidationErrorResponse response = new Gson().fromJson(json, ValidationErrorResponse.class);
                     message = String.join("\n", response.message());
                 } catch (Exception e2) {
