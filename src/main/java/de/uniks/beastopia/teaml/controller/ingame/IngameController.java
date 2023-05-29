2f5cfde04d3af89d4fe962c5735be5bad1b4efa9
Sylvan93 <macronix93@gmx.de>
2023-05-29 21:59:29 +0200

    feat: added little sprite preview in IngameController

--- src/main/java/de/uniks/beastopia/teaml/controller/ingame/IngameController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/IngameController.java
@@ -3,8 +3,11 @@ package de.uniks.beastopia.teaml.controller.ingame;
 import de.uniks.beastopia.teaml.App;
 import de.uniks.beastopia.teaml.controller.Controller;
 import de.uniks.beastopia.teaml.controller.menu.PauseController;
+import de.uniks.beastopia.teaml.service.PresetsService;
+import de.uniks.beastopia.teaml.service.TokenStorage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
@@ -23,7 +26,28 @@ public class IngameController extends Controller {
     Provider<PauseController> pauseControllerProvider;
 
     @Inject
+    TokenStorage tokenStorage;
+    @Inject
+    PresetsService presetsService;
+    @FXML
+    public ImageView showSprite;
+
+    @Inject
     public IngameController() {
+    }
+
+    @Override
+    public void init() {
+        super.init();
+
+        disposables.add(presetsService.getCharacterSprites(tokenStorage.getCurrentTrainer().image())
+                .observeOn(FX_SCHEDULER)
+                .subscribe(res -> {
+                    showSprite.setImage(res);
+                    showSprite.setPreserveRatio(true);
+                    showSprite.setSmooth(true);
+                    showSprite.setViewport(new javafx.geometry.Rectangle2D(48, 0, 16, 32));
+                }));
     }
 
     @FXML
