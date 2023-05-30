20c6e683f354dfc75d713e5232994664811cb7b0
Jonas Graunitz <jonas@graunitz.com>
2023-05-30 22:04:19 +0200

    refactor: removed warnings

--- src/main/java/de/uniks/beastopia/teaml/controller/menu/social/ChatGroupController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/menu/social/ChatGroupController.java
@@ -10,18 +10,14 @@ import de.uniks.beastopia.teaml.utils.Prefs;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.prefs.Preferences;
@@ -61,10 +57,6 @@ public class ChatGroupController extends Controller {
     @Inject
     public ChatGroupController() {
 
-    }
-
-    private static Image loadImage(URL imageUrl) throws FileNotFoundException, URISyntaxException {
-        return new Image(new FileInputStream(new File(imageUrl.toURI())));
     }
 
     @Override
