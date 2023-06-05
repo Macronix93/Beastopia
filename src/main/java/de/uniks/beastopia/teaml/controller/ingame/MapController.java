7b682d3ad4043533dc8ffa098c0017646e118f9a
Akloib <ali.k0beissi.ak@gmail.com>
2023-06-05 18:17:45 +0200

    chore:  add map controller and fxml

---
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/MapController.java
@@ -0,0 +1,14 @@
+package de.uniks.beastopia.teaml.controller.ingame;
+
+import de.uniks.beastopia.teaml.controller.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class MapController extends Controller {
+
+    @FXML
+    public Pane mapPane;
+    @FXML
+    public Button closeMap;
+}
