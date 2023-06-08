d30e4d8dd7091df49e5b2130d9be83d6680846d5
Akloib <ali.k0beissi.ak@gmail.com>
2023-06-08 23:58:08 +0200

    feat: draw map and shapes

--- src/main/java/de/uniks/beastopia/teaml/controller/ingame/MapController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/MapController.java
@@ -3,19 +3,27 @@ package de.uniks.beastopia.teaml.controller.ingame;
 import de.uniks.beastopia.teaml.App;
 import de.uniks.beastopia.teaml.controller.Controller;
 import de.uniks.beastopia.teaml.controller.menu.MenuController;
-import de.uniks.beastopia.teaml.rest.Region;
+import de.uniks.beastopia.teaml.rest.*;
 import de.uniks.beastopia.teaml.service.DataCache;
 import de.uniks.beastopia.teaml.service.PresetsService;
 import de.uniks.beastopia.teaml.service.RegionService;
 import de.uniks.beastopia.teaml.utils.LoadingPage;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
+import java.util.HashMap;
 
 public class MapController extends Controller {
+    private int TILE_SIZE = 16;
     @FXML
     public Pane mapPane;
     @Inject
@@ -30,7 +38,10 @@ public class MapController extends Controller {
     Provider<IngameController> ingameControllerProvider;
     private Region region;
     private IngameController backController;
-    private LoadingPage laodingPage;
+    private LoadingPage loadingPage;
+    private TileSet tileSet;
+    private Image image;
+    private Map map;
 
     // TODO: remove this backpass
     @Inject
@@ -47,16 +58,98 @@ public class MapController extends Controller {
 
     @Override
     public Parent render() {
-        laodingPage = LoadingPage.makeLoadingPage(super.render());
+        loadingPage = LoadingPage.makeLoadingPage(super.render());
         disposables.add(regionService.getRegion(cache.getJoinedRegion()._id())
                 .observeOn(FX_SCHEDULER)
                 .subscribe(region -> {
                             this.region = region;
-                            System.out.println(region);
-                            laodingPage.setDone();
-                        }
+                            this.map = region.map();
+                            this.tileSet = presetsService.getTileset(map.tilesets().get(0)).blockingFirst();
+                            this.image = presetsService.getImage(tileSet).blockingFirst();
+                            loadingPage.setDone();
+                            drawMap();
+                        },
+                        error -> System.out.println(error.getMessage())
                 ));
-        return laodingPage.parent();
+        return loadingPage.parent();
+    }
+
+    private void drawMap() {
+        for (Layer layer : this.map.layers()) {
+            System.out.println("layer names: " + layer.type());
+            if (layer.type().equals("tilelayer")) {
+                drawTileLayer(layer);
+            } else if (layer.type().equals("objectgroup")) {
+                drawObjectGroup(layer);
+            }
+
+        }
+    }
+
+    private void drawObjectGroup(Layer layer) {
+        for (MapObject object : layer.objects()) {
+            System.out.println("properties: " + object.properties().get(0).keySet());
+            if (object.polygon() == null) {
+                Rectangle r = new Rectangle();
+                r.setX(object.x());
+                r.setY(object.y());
+                r.setWidth(object.width());
+                r.setHeight(object.height());
+                r.setFill(javafx.scene.paint.Color.RED);
+                r.setOnMouseEntered(event -> {
+                    System.out.println("entered");
+                    r.setFill(Color.GREEN);
+                });
+                r.setOnMouseExited(event -> {
+                    System.out.println("exited");
+                    r.setFill(Color.RED);
+                });
+                mapPane.getChildren().add(r);
+            } else {
+                Polygon p = new Polygon();
+
+                for (HashMap<String, Double> point : object.polygon()) {
+                    double x = point.get("x") + object.x();
+                    double y = point.get("y") + object.y();
+                    p.getPoints().addAll(x, y);
+                }
+                p.fillProperty().setValue(Color.BLUEVIOLET);
+                p.setOnMouseEntered(event -> {
+                    System.out.println("entered");
+                    p.setFill(Color.GREEN);
+                });
+                p.setOnMouseExited(event -> {
+                    System.out.println("exited");
+                    p.setFill(Color.BLUEVIOLET);
+                });
+                mapPane.getChildren().add(p);
+            }
+
+        }
+    }
+
+    private void drawTileLayer(Layer layer) {
+        for (Chunk chunk : layer.chunks()) {
+            int chunkX = chunk.x();
+            int chunkY = chunk.y();
+            int index = 0;
+
+            for (int id : chunk.data()) {
+                int x = index % chunk.width() + chunkX;
+                int y = index / chunk.height() + chunkY;
+                index++;
+                ImageView view = new ImageView();
+                view.setPreserveRatio(true);
+                view.setSmooth(true);
+                view.setImage(image);
+                view.setFitWidth(TILE_SIZE + 1);
+                view.setFitHeight(TILE_SIZE + 1);
+                view.setViewport(presetsService.getTileViewPort(id, tileSet));
+                view.setTranslateX(x * TILE_SIZE);
+                view.setTranslateY(y * TILE_SIZE);
+                mapPane.getChildren().add(view);
+            }
+        }
     }
 
     @Override
@@ -70,7 +163,8 @@ public class MapController extends Controller {
 
         // TODO: remove this backpass
         MenuController menuController = menuControllerProvider.get();
-        app.show(backController);
+
+        app.show(menuController);
     }
 
     public void setRegion(Region region) {
@@ -79,5 +173,9 @@ public class MapController extends Controller {
 
     public void setBackController(IngameController ingameController) {
         this.backController = ingameController;
+    }
+
+    public void giveCor(MouseEvent mouseEvent) {
+        System.out.println("x: " + mouseEvent.getX() + " y: " + mouseEvent.getY());
     }
 }
