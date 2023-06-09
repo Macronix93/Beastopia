af97e80c9c0be2386a5462dc0bc2d6a49a639af9
Akloib <ali.k0beissi.ak@gmail.com>
2023-06-09 18:53:09 +0200

    test: add test classes

---
+++ src/test/java/de/uniks/beastopia/teaml/controller/ingame/MapControllerTest.java
@@ -0,0 +1,26 @@
+package de.uniks.beastopia.teaml.controller.ingame;
+
+import org.junit.jupiter.api.Test;
+
+class MapControllerTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void render() {
+    }
+
+    @Test
+    void destroy() {
+    }
+
+    @Test
+    void closeMap() {
+    }
+
+    @Test
+    void setRegion() {
+    }
+}
---
+++ src/test/java/de/uniks/beastopia/teaml/controller/ingame/RegionInfoControllerTest.java
@@ -0,0 +1,22 @@
+package de.uniks.beastopia.teaml.controller.ingame;
+
+import org.junit.jupiter.api.Test;
+
+class RegionInfoControllerTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void render() {
+    }
+
+    @Test
+    void destroy() {
+    }
+
+    @Test
+    void setText() {
+    }
+}
