b51eb823b8e610ddd642fbe67fee839320a79978
Lonartie <gierschner.lg@gmail.com>
2023-06-15 10:58:38 +0200

    fix: still use api v2

--- src/main/java/de/uniks/beastopia/teaml/Main.java
+++ src/main/java/de/uniks/beastopia/teaml/Main.java
@@ -3,8 +3,8 @@ package de.uniks.beastopia.teaml;
 import javafx.application.Application;
 
 public class Main {
-    public static final String API_URL = "https://stpmon.uniks.de/api/v3";
-    public static final String WS_URL = "wss://stpmon.uniks.de/ws/v3";
+    public static final String API_URL = "https://stpmon.uniks.de/api/v2";
+    public static final String WS_URL = "wss://stpmon.uniks.de/ws/v2";
     public static final String UDP_URL = "stpmon.uniks.de:30011";
 
     public static void main(String[] args) {
