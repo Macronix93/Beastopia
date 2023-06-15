7e0319b6c50ac825e2486b0ee9e46f637bacc539
Sylvan93 <macronix93@gmx.de>
2023-06-15 18:38:23 +0200

    feat: add sfx play functionality and test sound

--- src/main/java/de/uniks/beastopia/teaml/controller/ingame/SoundController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/SoundController.java
@@ -1,10 +1,14 @@
 package de.uniks.beastopia.teaml.controller.ingame;
 
+import de.uniks.beastopia.teaml.Main;
 import de.uniks.beastopia.teaml.controller.Controller;
 import de.uniks.beastopia.teaml.service.DataCache;
 import de.uniks.beastopia.teaml.utils.Prefs;
+import javafx.scene.media.AudioClip;
 
 import javax.inject.Inject;
+import java.net.URISyntaxException;
+import java.util.Objects;
 
 public class SoundController extends Controller {
     @Inject
@@ -12,11 +16,28 @@ public class SoundController extends Controller {
     @Inject
     Prefs prefs;
 
+    private AudioClip sfxPlayer;
+
     @Inject
     public SoundController() {
     }
 
     @Override
     public void init() {
+        super.init();
+    }
+
+    public void play(String fileName) {
+        try {
+            sfxPlayer = new AudioClip(Objects.requireNonNull(Main.class.getResource("assets/sounds/" + fileName)).toURI().toString());
+
+            double volumePercentage = prefs.getSoundVolume();
+            double volume = volumePercentage / 100.0;
+
+            sfxPlayer.setVolume(volume);
+            sfxPlayer.play();
+        } catch (URISyntaxException | NullPointerException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
