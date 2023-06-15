5fb2e7254dc0ba2a67754f66f42aa0b0f7c9fff1
Armin Kozik <armin.kozik@gmx.net>
2023-06-15 22:11:15 +0200

    test: add deleteUserController test basic structure

---
+++ src/test/java/de/uniks/beastopia/teaml/controller/menu/DeleteUserControllerTest.java
@@ -0,0 +1,71 @@
+package de.uniks.beastopia.teaml.controller.menu;
+
+import de.uniks.beastopia.teaml.App;
+import de.uniks.beastopia.teaml.controller.AppPreparer;
+import de.uniks.beastopia.teaml.controller.auth.LoginController;
+import de.uniks.beastopia.teaml.rest.User;
+import de.uniks.beastopia.teaml.service.AuthService;
+import de.uniks.beastopia.teaml.service.TokenStorage;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import javax.inject.Provider;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteUserControllerTest extends ApplicationTest {
+
+    @Spy
+    App app;
+    @InjectMocks
+    DeleteUserController deleteUserController;
+    @Mock
+    AuthService authService;
+    @Mock
+    TokenStorage tokenStorage;
+    @Mock
+    Provider<LoginController> loginControllerProvider;
+    @Mock
+    Provider<EditProfileController> editProfileControllerProvider;
+    @Spy
+    @SuppressWarnings("unused")
+    ResourceBundle resources = ResourceBundle.getBundle("de/uniks/beastopia/teaml/assets/lang", Locale.forLanguageTag("en"));
+
+    final User user = new User(null, null, "1", "1", null, null, null);
+
+    @Override
+    public void start(Stage stage) {
+        AppPreparer.prepare(app);
+
+        when(tokenStorage.getCurrentUser()).thenReturn(user);
+
+
+        app.start(stage);
+        app.show(deleteUserController);
+        stage.requestFocus();
+    }
+
+    @Test
+    public void deleteUser() {
+        clickOn("#passwordField");
+        write("12345678");
+        clickOn("#deleteUserTestId");
+    }
+
+
+    @Test
+    public void getTitle() {
+        assertEquals(app.getStage().getTitle(), resources.getString("titleDeleteUser"));
+    }
+
+}
