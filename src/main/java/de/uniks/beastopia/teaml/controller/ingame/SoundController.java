6764a6548681cb11d39a786e25051dd9ae1fa3b7
Sylvan93 <macronix93@gmx.de>
2023-06-15 19:31:28 +0200

    feat: improve play functionality

--- src/main/java/de/uniks/beastopia/teaml/controller/ingame/SoundController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/SoundController.java
@@ -5,8 +5,11 @@ import de.uniks.beastopia.teaml.controller.Controller;
 import de.uniks.beastopia.teaml.service.DataCache;
 import de.uniks.beastopia.teaml.utils.Prefs;
 import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 import javax.inject.Inject;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.Objects;
 
@@ -17,27 +20,38 @@ public class SoundController extends Controller {
     Prefs prefs;
 
     private AudioClip sfxPlayer;
+    private MediaPlayer bgmPlayer;
 
     @Inject
     public SoundController() {
     }
 
-    @Override
-    public void init() {
-        super.init();
-    }
-
     public void play(String fileName) {
         try {
-            sfxPlayer = new AudioClip(Objects.requireNonNull(Main.class.getResource("assets/sounds/" + fileName)).toURI().toString());
+            if (fileName.startsWith("bgm:")) {
+                if (bgmPlayer != null) {
+                    bgmPlayer.stop();
+                }
+                Media bgmMedia = new Media(Objects.requireNonNull(Main.class.getResource("assets/sounds/bgm_" + fileName.substring(4) + ".mp3")).toURI().toURL().toString());
+                bgmPlayer = new MediaPlayer(bgmMedia);
 
-            double volumePercentage = prefs.getSoundVolume();
-            double volume = volumePercentage / 100.0;
+                double volumePercentage = prefs.getMusicVolume();
+                double volume = volumePercentage / 100.0;
 
-            sfxPlayer.setVolume(volume);
-            sfxPlayer.play();
-        } catch (URISyntaxException | NullPointerException e) {
-            throw new RuntimeException(e);
+                bgmPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+                bgmPlayer.setVolume(volume);
+                bgmPlayer.play();
+            } else if (fileName.startsWith("sfx:")) {
+                sfxPlayer = new AudioClip(Objects.requireNonNull(Main.class.getResource("assets/sounds/sfx_" + fileName.substring(4) + ".mp3")).toURI().toString());
+
+                double volumePercentage = prefs.getSoundVolume();
+                double volume = volumePercentage / 100.0;
+
+                sfxPlayer.setVolume(volume);
+                sfxPlayer.play();
+            }
+        } catch (URISyntaxException | NullPointerException | MalformedURLException e) {
+            e.printStackTrace();
         }
     }
 }
