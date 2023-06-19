b9bf55d34e6e7b9c591f565da70d5b26a1a689f3
Armin Kozik <armin.kozik@gmx.net>
2023-06-19 15:28:28 +0200

    feat: create encounter.fxml

---
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/EncounterController.java
@@ -0,0 +1,13 @@
+package de.uniks.beastopia.teaml.controller.ingame;
+
+import de.uniks.beastopia.teaml.controller.Controller;
+
+import javax.inject.Inject;
+
+public class EncounterController extends Controller {
+
+    @Inject
+    public EncounterController() {
+    }
+
+}
