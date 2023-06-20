01ee0b45fa6fb5cee02a6380252c33f26a5bd20e
Akloib <ali.k0beissi.ak@gmail.com>
2023-06-20 18:11:07 +0200

    feat: add init structure of EditBeast functionality

---
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/beast/EditBeastTeamController.java
@@ -0,0 +1,10 @@
+package de.uniks.beastopia.teaml.controller.ingame.beast;
+
+import de.uniks.beastopia.teaml.controller.Controller;
+import javafx.scene.input.InputMethodEvent;
+
+public class EditBeastTeamController extends Controller {
+
+    public void filterMonster(InputMethodEvent inputMethodEvent) {
+    }
+}
