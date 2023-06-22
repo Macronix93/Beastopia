09b6553f256c047b26e5ea0986b140bcde8d59ee
Luis Schrader <luisschr@protonmail.com>
2023-06-22 13:58:18 +0200

    feat: Add Variant for move (oneOf)

--- src/main/java/de/uniks/beastopia/teaml/Main.java
+++ src/main/java/de/uniks/beastopia/teaml/Main.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 public class Main {
     public static final String API_URL = "https://stpmon.uniks.de/api/v3";
     public static final String WS_URL = "wss://stpmon.uniks.de/ws/v3";
-    public static final String UDP_URL = "stpmon.uniks.de:30011";
+    public static final String UDP_URL = "stpmon.uniks.de:30013";
 
     public static void main(String[] args) {
         Application.launch(App.class, args);
--- src/main/java/de/uniks/beastopia/teaml/rest/UpdateOpponentDto.java
+++ src/main/java/de/uniks/beastopia/teaml/rest/UpdateOpponentDto.java
@@ -1,5 +1,7 @@
 package de.uniks.beastopia.teaml.rest;
 
+import de.uniks.beastopia.teaml.utils.Variant;
+
 public record UpdateOpponentDto(
         String monster,
         Move move
