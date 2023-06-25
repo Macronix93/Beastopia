fd10d27af6026581fe58baa3d93a486a3295435b
Celina Werkmeister <uk083378@student.uni-kassel.de>
2023-06-25 13:37:35 +0200

    feat: create methods in DialogWindowController.java

--- src/main/java/de/uniks/beastopia/teaml/controller/ingame/DialogWindowController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/DialogWindowController.java
@@ -3,22 +3,66 @@ package de.uniks.beastopia.teaml.controller.ingame;
 import de.uniks.beastopia.teaml.controller.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class DialogWindowController extends Controller {
 
     @FXML
-    Text dialogWindow;
+    Text dialogText;
+    @FXML
+    VBox imageBox;
+    @FXML
+    HBox choiceBox;
+    @FXML
+    ImageView imageView;
+    @Inject
+    Provider<ResourceBundle> resourcesProvider;
+    @Inject
+    Provider<IngameController> ingameControllerProvider;
 
     @Override
     public String getTitle() {
-        return resources.getString("titleDialogWindow");
+        return "titleDialogWindow";
+    }
+
+    public void start() {
+        Image image = new Image("");
+        imageView = new ImageView(image);
+        imageView.fitWidthProperty().bind(imageBox.widthProperty());
+        imageView.fitHeightProperty().bind(imageBox.heightProperty());
     }
 
     @Override
     public Parent render() {
         Parent parent = super.render();
 
+
         return parent;
+    }
+
+    public void setChoices(List<String> choices) {
+        for (String choice : choices) {
+            Button button = new Button(choice);
+            choiceBox.getChildren().add(button);
+        }
+    }
+
+    public void setImage(Image image) {
+        imageView.setImage(image);
+    }
+
+    @FXML
+    public void back() {
+        app.show(ingameControllerProvider.get());
     }
 }
