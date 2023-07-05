4ea71508bf71e0b44e916232b880a7f34876bae2
arkozik <arkozik@student.uni-kassel.de>
2023-07-05 14:14:34 +0200

    feat: basic structure of controller and fxml

---
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/encounter/EndScreenController.java
@@ -0,0 +1,55 @@
+package de.uniks.beastopia.teaml.controller.ingame.encounter;
+
+import de.uniks.beastopia.teaml.controller.Controller;
+import de.uniks.beastopia.teaml.rest.Monster;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+
+public class EndScreenController extends Controller {
+
+    @FXML
+    Button leaveButton;
+
+    private Monster winner1;
+    private Monster winner2;
+    private Monster loser1;
+    private Monster loser2;
+
+    @Inject
+    public EndScreenController() {
+    }
+
+    @Override
+    public void init() {
+        super.init();
+
+    }
+
+    @Override
+    public Parent render() {
+        Parent parent = super.render();
+
+        return parent;
+    }
+
+    public void setWinner1(Monster winner1) {
+        this.winner1 = winner1;
+    }
+
+    public void setWinner2(Monster winner2) {
+        this.winner2 = winner2;
+    }
+
+    public void setLoser1(Monster loser1) {
+        this.loser1 = loser1;
+    }
+
+    public void setLoser2(Monster loser2) {
+        this.loser2 = loser2;
+    }
+
+}
