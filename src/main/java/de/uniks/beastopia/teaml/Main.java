712b876e6ee0e1f810823f912290138784bfa580
Celina <werkmeisterc@t-online.de>
2023-07-18 13:48:56 +0200

    feat: change URLs for 4th Release

--- src/main/java/de/uniks/beastopia/teaml/Main.java
+++ src/main/java/de/uniks/beastopia/teaml/Main.java
@@ -3,9 +3,9 @@ package de.uniks.beastopia.teaml;
 import javafx.application.Application;
 
 public class Main {
-    public static final String API_URL = "https://stpmon.uniks.de/api/v3";
-    public static final String WS_URL = "wss://stpmon.uniks.de/ws/v3";
-    public static final String UDP_URL = "stpmon.uniks.de:30013";
+    public static final String API_URL = "https://stpmon.uniks.de/api/v4";
+    public static final String WS_URL = "wss://stpmon.uniks.de/ws/v4";
+    public static final String UDP_URL = "stpmon.uniks.de:30014";
 
     public static void main(String[] args) {
         Application.launch(App.class, args);
