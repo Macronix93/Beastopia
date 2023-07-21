b283522aaac7f7a4ef91c1ee7f4c0fa5d5a697ee
Jonas Graunitz <jonas@graunitz.com>
2023-07-21 14:23:17 +0200

    feat: added image

--- src/main/java/de/uniks/beastopia/teaml/controller/ingame/MondexElementController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/MondexElementController.java
@@ -3,6 +3,7 @@ package de.uniks.beastopia.teaml.controller.ingame;
 import de.uniks.beastopia.teaml.controller.Controller;
 import de.uniks.beastopia.teaml.rest.Monster;
 import de.uniks.beastopia.teaml.rest.MonsterTypeDto;
+import de.uniks.beastopia.teaml.service.PresetsService;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
@@ -11,6 +12,8 @@ import javax.inject.Inject;
 
 public class MondexElementController extends Controller{
 
+    @Inject
+    PresetsService presetsService;
     public ImageView imageView_avatar;
     public Label label_name;
     public Label label_id;
@@ -32,6 +35,11 @@ public class MondexElementController extends Controller{
         Parent parent = super.render();
         label_name.setText(monster.name());
         label_id.setText("#" + (monster.id()));
+
+        disposables.add(presetsService.getMonsterImage(monster.id())
+                .observeOn(FX_SCHEDULER)
+                .subscribe(monsterImage -> imageView_avatar.setImage(monsterImage)));
+
         return parent;
     }
 }
