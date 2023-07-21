02f22a09046098f9d9a5c7ece244aa46f06f38fa
Luis Schrader <luisschr@protonmail.com>
2023-07-21 11:40:58 +0200

    fix: load achievements

--- src/main/java/de/uniks/beastopia/teaml/controller/ingame/ScoreboardController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/ScoreboardController.java
@@ -61,7 +61,7 @@ public class ScoreboardController extends Controller {
     public Parent render() {
         Parent parent = super.render();
         achievements.setVisible(clicked);
-        //loadAchievements();
+        loadAchievements();
         return parent;
     }
 
