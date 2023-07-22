d2d3c43bc3eb51488bf4f5fb0ae080cef6fbb165
Jonas Graunitz <jonas@graunitz.com>
2023-07-22 16:08:34 +0200

    fix: fixes test

--- src/test/java/de/uniks/beastopia/teaml/controller/ingame/IngameControllerTest.java
+++ src/test/java/de/uniks/beastopia/teaml/controller/ingame/IngameControllerTest.java
@@ -134,7 +134,7 @@ class IngameControllerTest extends ApplicationTest {
         doNothing().when(beastListController).init();
         when(beastListController.render()).thenReturn(new Pane());
         doNothing().when(mondexListController).setOnCloseRequest(any());
-        //doNothing().when(mondexListController).setOnBeastClicked(any());
+        doNothing().when(mondexListController).setOnBeastClicked(any());
         doNothing().when(mondexListController).init();
         when(mondexListController.render()).thenReturn(new Pane());
         when(soundControllerProvider.get()).thenReturn(soundController);
