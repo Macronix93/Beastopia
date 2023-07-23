bb2075690ae87364d32c36a8dd7556cbbf0ebf25
Sylvan93 <macronix93@gmx.de>
2023-07-23 18:08:09 +0200

    chore: add getter for enemy monster

--- src/main/java/de/uniks/beastopia/teaml/controller/ingame/encounter/EnemyBeastInfoController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/ingame/encounter/EnemyBeastInfoController.java
@@ -65,4 +65,8 @@ public class EnemyBeastInfoController extends Controller {
         lifeBarValue.setMaxWidth(lifeBar.getWidth() * value);
         lifeBarValue.setPrefWidth(lifeBar.getWidth() * value);
     }
+
+    public Monster getMonster() {
+        return this.monster;
+    }
 }
