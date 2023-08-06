3389bac41fbc2849cfb2a355b82d62236d10ce6a
Jonas Graunitz <jonas@graunitz.com>
2023-08-06 13:23:45 +0200

    feat: added theme to pause buttons

--- src/main/java/de/uniks/beastopia/teaml/controller/menu/PauseController.java
+++ src/main/java/de/uniks/beastopia/teaml/controller/menu/PauseController.java
@@ -1,22 +1,37 @@
 package de.uniks.beastopia.teaml.controller.menu;
 
+import de.uniks.beastopia.teaml.Main;
 import de.uniks.beastopia.teaml.controller.Controller;
 import de.uniks.beastopia.teaml.controller.ingame.TrainerController;
 import de.uniks.beastopia.teaml.controller.ingame.beast.EditBeastTeamController;
 import de.uniks.beastopia.teaml.controller.menu.social.FriendListController;
+import de.uniks.beastopia.teaml.service.ImageService;
 import de.uniks.beastopia.teaml.utils.SoundController;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PauseController extends Controller {
 
     private final List<Controller> subControllers = new ArrayList<>();
+    @FXML
+    public ImageView settingsImage;
+    @FXML
+    public ImageView editProfileImage;
+    @FXML
+    public ImageView backToMainImage;
+    @FXML
+    public ImageView trainerMenuImage;
+    @FXML
+    public ImageView beastTeamImage;
     @Inject
     Provider<FriendListController> friendListControllerProvider;
     @Inject
@@ -31,6 +46,8 @@ public class PauseController extends Controller {
     Provider<EditBeastTeamController> editBeastTeamControllerProvider;
     @Inject
     Provider<SoundController> soundControllerProvider;
+    @Inject
+    ImageService imageService;
 
     @FXML
     private VBox friendListContainer;
@@ -47,6 +64,13 @@ public class PauseController extends Controller {
         Controller subController = friendListControllerProvider.get();
         subControllers.add(subController);
         friendListContainer.getChildren().add(subController.render());
+
+        settingsImage.setImage(imageService.getThemeImage(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("assets/buttons/settings.png")))));
+        editProfileImage.setImage(imageService.getThemeImage(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("assets/buttons/edit.png")))));
+        backToMainImage.setImage(imageService.getThemeImage(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("assets/buttons/back.png")))));
+        trainerMenuImage.setImage(imageService.getThemeImage(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("assets/trainer.png")))));
+        beastTeamImage.setImage(imageService.getThemeImage(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("assets/buttons/beast.png")))));
+
         return parent;
     }
 
