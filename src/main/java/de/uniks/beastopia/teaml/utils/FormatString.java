1863fffcd430c67b0774f47a8ad57f4bb548a0bc
schryxcz <luisschr@protonmail.com>
2023-08-10 13:20:31 +0200

    fix: formatString in itemDetails

--- src/main/java/de/uniks/beastopia/teaml/utils/FormatString.java
+++ src/main/java/de/uniks/beastopia/teaml/utils/FormatString.java
@@ -1,24 +1,16 @@
 package de.uniks.beastopia.teaml.utils;
 
 public class FormatString {
-
-    public static String formatString(String itemName, int length) {
-        if (itemName.length() > length) {
-            int lastSpace = itemName.lastIndexOf(' ', length);
-            if (lastSpace != -1) { // \n after last space
-                if (itemName.length() > length * 2) {
-                    String lastPart = itemName.substring(lastSpace + length);
-                    return itemName.substring(0, lastSpace) + "\n" + formatString(lastPart, length);
-                }
-                return itemName.substring(0, lastSpace) + "\n" + formatString(itemName.substring(lastSpace + 1), length);
-            } else { // if too long and no space
-                if (itemName.length() > length * 2) {
-                    String lastPart = itemName.substring(length);
-                    return itemName.substring(0, 12) + "-\n" + formatString(lastPart, length);
-                }
-                return itemName.substring(0, 12) + "-\n" + itemName.substring(length);
-            }
+    public static String formatString(String stringToFormat, int length) {
+        if (stringToFormat.length() <= length) {
+            return stringToFormat;
         }
-        return itemName;
+
+        int lastSpace = stringToFormat.lastIndexOf(' ', length);
+        if (lastSpace != -1) {
+            return stringToFormat.substring(0, lastSpace) + "\n" + formatString(stringToFormat.substring(lastSpace + 1), length);
+        } else {
+            return stringToFormat.substring(0, length) + "-\n" + formatString(stringToFormat.substring(length), length);
+        }
     }
 }
